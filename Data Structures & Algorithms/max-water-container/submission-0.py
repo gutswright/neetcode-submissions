180fc308b30ae6e2e5ed3b1e2981ddf609ae0674
Joshua Wright <132239430+gutswright@users.noreply.github.com>
2026-07-31 15:34:20 -0600

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        hashmap = {i: heights[i] for i in range(len(heights))}
+        print({key: value for key, value in hashmap.items()})
+        l, r = 0, len(heights) - 1
+        curMax = 0
+        for i in range(len(heights)):
+            print(heights[l], heights[r])
+            curMax = max(min(heights[l],heights[r]) * (r-l), curMax) 
+            if heights[r] > heights[l]:
+                l += 1
+            elif heights[r] < heights[l]:
+                r -= 1
+            else:
+                r -= 1
+        print(heights[l], heights[r])
+        return curMax
+        
